b337ed501cc1c60e110d2989a298934582b050c2
Darshiney19 <134794005+Darshiney19@users.noreply.github.com>
2023-06-09 12:38:35 +0530

    Add files via upload

---
+++ CABS.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+
+class Codechef
+{
+    public static void main (String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            int X=sc.nextInt();
+            int Y=sc.nextInt();
+            if(X<Y)
+            {
+                System.out.println("FIRST");
+            }
+            else if (X==Y)
+            {
+                System.out.println("ANY");
+            }
+            else
+            {
+                System.out.println("SECOND");
+            }
+        }
+    }
+}
